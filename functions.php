1b08456a6e52826c6e38b97c35a0246c65f4693d
Weiki886 <weiki886@163.com>
2025-03-13 23:11:52 +0800

    重新修改标签跳转

--- functions.php
+++ functions.php
@@ -7,7 +7,15 @@
     include_once(get_template_directory() . '/inc/common_setup.php');
     include_once(get_template_directory() . '/inc/extra_setup.php');
 
-   
+    // 添加标签链接过滤器，确保所有标签链接都包含index.php
+    add_filter('tag_link', 'modify_tag_link', 10, 2);
+    function modify_tag_link($tag_link, $tag_id) {
+        // 检查链接中是否已包含index.php，如果没有则添加
+        if (strpos($tag_link, '/index.php/') === false) {
+            return str_replace('/tag/', '/index.php/tag/', $tag_link);
+        }
+        return $tag_link;
+    }
 
     /*--------------------------------------------------------------------------
      * ====== THEME PANEL CONTROLS ======
