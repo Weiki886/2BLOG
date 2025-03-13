58c944012a10b4d3871e80c5320fcfa05eab336d
Weiki886 <weiki886@163.com>
2025-03-13 23:05:03 +0800

    修改页面跳转标签

--- functions.php
+++ functions.php
@@ -6,14 +6,19 @@
     */
     include_once(get_template_directory() . '/inc/common_setup.php');
     include_once(get_template_directory() . '/inc/extra_setup.php');
+
+    // 添加标签链接过滤器
+    add_filter('tag_link', 'modify_tag_link', 10, 2);
+    function modify_tag_link($tag_link, $tag_id) {
+        return str_replace('/tag/', '/index.php/tag/', $tag_link);
+    }
+
     /*--------------------------------------------------------------------------
      * ====== THEME PANEL CONTROLS ======
      * https://themes.artbees.net/blog/custom-setting-page-in-wordpress/
      * -------------------------------------------------------------------------
     */
     if(is_admin()) {
-        // load_theme_partial('/inc/theme_settings.php');
         include_once(get_template_directory() . '/inc/theme_settings.php');
-        // print_r(get_cat_by_template('weblog'));
     }
 ?>
